f16104b2f4140804979506d63fb9767423105416
hullch <hullc503@gmail.com>
2017-06-12 13:08:39 -0700

    Updated MainActivity to handle preferences

--- CryptoCurrencyApp/app/src/main/java/com/example/cryptocurrencyapp/MainActivity.java
+++ CryptoCurrencyApp/app/src/main/java/com/example/cryptocurrencyapp/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.cryptocurrencyapp;
 import com.example.cryptocurrencyapp.utils.*;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -16,7 +18,8 @@ import android.widget.TextView;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements CryptoCurrencyAdapter.OnCryptoItemClickListener {
+public class MainActivity extends AppCompatActivity implements CryptoCurrencyAdapter.OnCryptoItemClickListener,
+        SharedPreferences.OnSharedPreferenceChangeListener {
 
     private RecyclerView mCryptoListRV;
     private CryptoCurrencyAdapter mcryptoCurrencyAdapter;
@@ -43,17 +46,51 @@ public class MainActivity extends AppCompatActivity implements CryptoCurrencyAda
         mcryptoCurrencyAdapter = new CryptoCurrencyAdapter(this);
         mCryptoListRV.setAdapter(mcryptoCurrencyAdapter);
 
-        doCryptonatorSearch();
+
+        doCryptonatorSearch(getCurrencies());
     }
 
-    private void doCryptonatorSearch() {
+
+    private ArrayList<String> getCurrencies(){
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(this);
+
+        ArrayList<String> currencies = new ArrayList<>();
+
+        boolean prefBTC = sharedPreferences.getBoolean(
+                getString(R.string.pref_btc_key), true);
+        boolean prefETH = sharedPreferences.getBoolean(
+                getString(R.string.pref_eth_key), false);
+        boolean prefLTC = sharedPreferences.getBoolean(
+                getString(R.string.pref_ltc_key), false);
+        boolean prefXMR = sharedPreferences.getBoolean(
+                getString(R.string.pref_xmr_key), false);
+        boolean prefXRP = sharedPreferences.getBoolean(
+                getString(R.string.pref_xrp_key), false);
+        boolean prefDOGE = sharedPreferences.getBoolean(
+                getString(R.string.pref_doge_key), true);
+
+        if(prefBTC) currencies.add("btc-usd");
+        if(prefETH) currencies.add("eth-usd");
+        if(prefLTC) currencies.add("ltc-usd");
+        if(prefXMR) currencies.add("xmr-usd");
+        if(prefXRP) currencies.add("xrp-usd");
+        if(prefDOGE) currencies.add("doge-usd");
+
+        return currencies;
+
+    }
+
+    private void doCryptonatorSearch(ArrayList<String> currencies) {
         ArrayList<String> cryptonatorURLs = new ArrayList<>();
-        cryptonatorURLs.add(CryptonatorUtils.buildCryptonatorURL("btc-usd"));
-        cryptonatorURLs.add(CryptonatorUtils.buildCryptonatorURL("eth-usd"));
-        cryptonatorURLs.add(CryptonatorUtils.buildCryptonatorURL("ltc-usd"));
-        cryptonatorURLs.add(CryptonatorUtils.buildCryptonatorURL("xmr-usd"));
-        cryptonatorURLs.add(CryptonatorUtils.buildCryptonatorURL("xrp-usd"));
-        cryptonatorURLs.add(CryptonatorUtils.buildCryptonatorURL("doge-usd"));
+        for(String currency : currencies) {
+            cryptonatorURLs.add(CryptonatorUtils.buildCryptonatorURL(currency));
+            //cryptonatorURLs.add(CryptonatorUtils.buildCryptonatorURL("eth-usd"));
+            //cryptonatorURLs.add(CryptonatorUtils.buildCryptonatorURL("ltc-usd"));
+            //cryptonatorURLs.add(CryptonatorUtils.buildCryptonatorURL("xmr-usd"));
+            //cryptonatorURLs.add(CryptonatorUtils.buildCryptonatorURL("xrp-usd"));
+            //cryptonatorURLs.add(CryptonatorUtils.buildCryptonatorURL("doge-usd"));
+        }
         Log.d("MainActivity", "got search url: " + cryptonatorURLs.toString());
         new CryptoCurrencySearchTask().execute(cryptonatorURLs);
     }
@@ -63,6 +100,11 @@ public class MainActivity extends AppCompatActivity implements CryptoCurrencyAda
         Intent intent = new Intent(this, CryptoCurrencyDetailActivity.class);
         intent.putExtra(CryptonatorUtils.CryptoCurrencyItem.EXTRA_CRYPTO_RESULT, cryptoCurrencyItem);
         startActivity(intent);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        doCryptonatorSearch(getCurrencies());
     }
 
 
